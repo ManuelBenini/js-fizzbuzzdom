fbab25a2209dbc81f664bf31a2cb7ce4f802c15f
ManuelBenini <manuelbenini1905@gmail.com>
2022-04-08 13:10:29 +0200

    JS Workflow

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*
+  a.  Scrivere un programma che stampi i numeri da 1 a 100,
+  b.  Per i multipli di 3 stampare Fizz,
+  c.  Per i multipli di 5 stampare Buzz,
+  d.  Per i multi di 3 e 5 stampare FizzBuzz,
+  f.  Creare stili differenti per le 3 situazioni sovrastanti.
+
+  1.  Dichiarare le variabili,
+  2.  Creare un ciclo For con start 1 e limite 100,
+  3.  Aggiungere le condizioni IF per i multipli con aggiunta classi,
+  4.  Scrivere dentro il container nel DOM i vari div.
+*/
