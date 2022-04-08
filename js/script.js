f2b5a5044a194a82708e66f01035581311d64ff2
ManuelBenini <manuelbenini1905@gmail.com>
2022-04-08 13:31:33 +0200

    Esercizio terminato

--- js/script.js
+++ js/script.js
@@ -10,3 +10,32 @@
   3.  Aggiungere le condizioni IF per i multipli con aggiunta classi,
   4.  Scrivere dentro il container nel DOM i vari div.
 */
+
+const container = document.querySelector('#container');
+
+const limit = 100;
+
+for (let i = 1; i <= limit; i++){
+  const defaultBox = document.createElement("div");
+  defaultBox.className = "box";
+  defaultBox.append(i)
+
+  //multiplo di 3
+  if (i % 3 === 0){
+    defaultBox.classList.add("box-three")
+    console.log(i);
+  }
+
+  //multiplo di 5
+  if (i % 5 === 0){
+    defaultBox.classList.add("box-five")
+    console.log(i);
+  }
+
+  //multiplo di 3 e 5
+  if (i % 3 === 0 && i % 5 === 0){
+    defaultBox.classList.add("box-union")
+  }
+  
+  container.append(defaultBox)
+}
